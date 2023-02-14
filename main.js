9d4b66d6fb7afe18981e1b3437bf79fd8ae04ef7
ivansirianni <108744343+ivansirianni@users.noreply.github.com>
2023-02-14 10:21:35 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -4,3 +4,10 @@ document.addEventListener('DOMContentLoaded', function () {
     options.html = "<div style='background: red;'>jdskljdklsjdkl</div>";
     var instances = M.Tooltip.init(elems, options);
 });
+
+document.addEventListener('DOMContentLoaded', function() {
+    var elems = document.querySelectorAll('.sidenav');
+    var options = new Object();
+    options.html = "<div style='background:yellow;'></div>";
+    var instances = M.Sidenav.init(elems, options);
+  });
